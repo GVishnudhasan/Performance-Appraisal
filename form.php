8abc79a515415f4de17ccf43c3c18b68bbac9759
Sowndarya19 <112232903+Sowndarya19@users.noreply.github.com>
2022-08-29 07:12:37 +0530

    Add files via upload

--- form.php
+++ form.php
@@ -9,6 +9,7 @@
 	$mobileno=$_POST['mobileno'];
 	$email=$_POST['email'];
 	$password=$_POST['password'];
+	$cpassword=$_POST['cpassword'];
 
 
 
@@ -17,17 +18,18 @@
 
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','database123');
+	$conn = new mysqli('localhost','root','','database01');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into form1_details(facultyid,Name,department,designation,totalexperience,dob,doj,mobileno,email,password) values(?,?,?,?,?,?,?,?,?,?)");
-		$stmt->bind_param("ssssississ", $facultyid, $Name,$Department,$designation,$totalexperience,$dob,$doj,$mobileno,$email,$password);
+		$stmt = $conn->prepare("insert into form2_details(facultyid,Name,department,designation,totalexperience,dob,doj,mobileno,email,password,cpassword) values(?,?,?,?,?,?,?,?,?,?,?)");
+		$stmt->bind_param("ssssississs", $facultyid, $Name,$Department,$designation,$totalexperience,$dob,$doj,$mobileno,$email,$password,$cpassword);
 		$execval = $stmt->execute();
-		echo $execval;  
+		echo $execval; 
 		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
 	}
+
 ?>
