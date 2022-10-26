33fc32bca127045922ff9f443282a81828bfc695
GVishnudhasan <gvishnud10@gmail.com>
2022-10-26 23:50:54 +0530

    update

--- DemoApp/frontend/src/app/lt5/lt5.component.ts
+++ DemoApp/frontend/src/app/lt5/lt5.component.ts
@@ -9,7 +9,47 @@ export class Lt5Component implements OnInit {
 
   constructor() { }
 
+
+
+
+  categoryList = {
+    g1 : {
+      qty_of_class :{
+        os1:0,
+        os2:0,
+        es1:0,
+        es2:0,
+        sum: 0,
+        overall_total:20
+      },
+      qb:{
+        os1:0,
+        os2:0,
+        es1:0,
+        es2:0,
+        sum: 0,
+        overall_total:10
+      },
+      rf_book:{
+        os1:0,
+        os2:0,
+        es1:0,
+        es2:0,
+        sum: 0,
+        overall_total:10
+    }
+  }}
+
   ngOnInit(): void {
+  }
+
+  async setmap(){
+    await this.opt_data();
+    console.log(this.categoryList);
+  }
+
+  async opt_data(){
+    this.categoryList.g1.qty_of_class.sum =  this.categoryList.g1.qty_of_class.os1 +  this.categoryList.g1.qty_of_class.os2 +  this.categoryList.g1.qty_of_class.es1 +  this.categoryList.g1.qty_of_class.es2;
   }
 
 }
