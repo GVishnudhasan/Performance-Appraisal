db521338f0413c41503f3acd6a01aaa49e92904b
Sowndarya19 <112232903+Sowndarya19@users.noreply.github.com>
2022-11-25 09:53:56 +0530

    changed files

--- DemoApp/backend/controllers/mController.js
+++ DemoApp/backend/controllers/mController.js
@@ -1,18 +1,24 @@
 
 //const mDb = require('../service/database');
 
+const jwt=require('jsonwebtoken')
 exports.login = async(req, res, next) => {
     try {
+        console.log(req.body);
         var result = {};
         let data = req.body.data.data;
+        
         if (data.email === 'admin' && data.password === 'admin') {
-            console.log("data >>", data);
+            console.log("data >>",process.env.ACCESS_TOKEN_SECRET, data);
+            var token="";
             result = {
-                token : "Bearer <token>",
+                
                 user_name: "testuser",
                 user_type: "string",
                 login_id: 1
             }
+            token= jwt.sign(result,'dev@123');
+            result['token']="Bearer"+ token ;
         } else {
             throw new Error('Auth Error1');
         }
