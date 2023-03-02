dcfbfb1d54a801bbaa782c00ab85aa7eafaee94e
GVishnudhasan <gvishnud10@gmail.com>
2023-03-02 16:15:11 +0530

    PDF

--- DemoApp/backend/controllers/mController.js
+++ DemoApp/backend/controllers/mController.js
@@ -3,6 +3,9 @@ const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+// const passwordResetToken = require('../models/resettoken');
 
 const User = require("../model/User");
 
@@ -128,3 +131,107 @@ exports.signup = async (req, res, next) => {
         res.status(500).send("Error in Saving");
     }
 }
+
+// exports.ResetPassword = async (req, res) => {
+//     if (!req.body.email) {
+//         return res
+//             .status(500)
+//             .json({ message: 'Email is required' });
+//     }
+//     const user = await User.findOne({
+//         email: req.body.email
+//     });
+//     if (!user) {
+//         return res
+//             .status(409)
+//             .json({ message: 'Email does not exist' });
+//     }
+//     var resettoken = new passwordResetToken({ _userId: user._id, resettoken: crypto.randomBytes(16).toString('hex') });
+//     resettoken.save(function (err) {
+//         if (err) { return res.status(500).send({ msg: err.message }); }
+//         passwordResetToken.find({ _userId: user._id, resettoken: { $ne: resettoken.resettoken } }).remove().exec();
+//         res.status(200).json({ message: 'Reset Password successfully.' });
+//         var transporter = nodemailer.createTransport({
+//             service: 'Gmail',
+//             port: 465,
+//             auth: {
+//                 user: 'user',
+//                 pass: 'password'
+//             }
+//         });
+//         var mailOptions = {
+//             to: user.email,
+//             from: 'your email',
+//             subject: 'Node.js Password Reset',
+//             text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+//                 'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+//                 'http://localhost:4200/response-reset-password/' + resettoken.resettoken + '\n\n' +
+//                 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+//         }
+//         transporter.sendMail(mailOptions, (err, info) => {
+//         })
+//     })
+// },
+
+// exports.ValidPasswordToken = async (req, res) => {
+//     if (!req.body.resettoken) {
+//         return res
+//             .status(500)
+//             .json({ message: 'Token is required' });
+//     }
+//     const user = await passwordResetToken.findOne({
+//         resettoken: req.body.resettoken
+//     });
+//     if (!user) {
+//         return res
+//             .status(409)
+//             .json({ message: 'Invalid URL' });
+//     }
+//     User.findOneAndUpdate({ _id: user._userId }).then(() => {
+//         res.status(200).json({ message: 'Token verified successfully.' });
+//     }).catch((err) => {
+//         return res.status(500).send({ msg: err.message });
+//     });
+// },
+
+// exports.NewPassword = async (req, res) => {
+//     passwordResetToken.findOne({ resettoken: req.body.resettoken }, function (err, userToken, next) {
+//         if (!userToken) {
+//             return res
+//                 .status(409)
+//                 .json({ message: 'Token has expired' });
+//         }
+
+//         User.findOne({
+//             _id: userToken._userId
+//         }, function (err, userEmail, next) {
+//             if (!userEmail) {
+//                 return res
+//                     .status(409)
+//                     .json({ message: 'User does not exist' });
+//             }
+//             return bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+//                 if (err) {
+//                     return res
+//                         .status(400)
+//                         .json({ message: 'Error hashing password' });
+//                 }
+//                 userEmail.password = hash;
+//                 userEmail.save(function (err) {
+//                     if (err) {
+//                         return res
+//                             .status(400)
+//                             .json({ message: 'Password can not reset.' });
+//                     } else {
+//                         userToken.remove();
+//                         return res
+//                             .status(201)
+//                             .json({ message: 'Password reset successfully' });
+//                     }
+
+//                 });
+//             });
+//         });
+
+//     })
+// }
--- DemoApp/backend/routes/mRoutes.js
+++ DemoApp/backend/routes/mRoutes.js
@@ -6,6 +6,9 @@ const auth = require("../authServer");
 
 router.post("/login", Controller.login);
 router.post("/signup", Controller.signup);
+// router.post('/req-reset-password', auth.ResetPassword);
+// router.post('/new-password', auth.NewPassword);
+// router.post('/valid-password-token', auth.ValidPasswordToken);
 router.get("/show", auth, async (req, res) => {
     try {
       // request.user is getting fetched from Middleware after token authentication
