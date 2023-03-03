fce8ef7556c161773c1cab4f9767fe56e2949f43
Sowndarya19 <112232903+Sowndarya19@users.noreply.github.com>
2023-03-03 09:47:48 +0530

    Change

--- DemoApp/backend/controllers/mController.js
+++ DemoApp/backend/controllers/mController.js
@@ -14,8 +14,28 @@ exports.login = async (req, res, next) => {
     const { facultyid, password } = await req.body;
     // console.log("hello", facultyid, password, req.body);
     try {
+<<<<<<< Updated upstream
         let user = await User.findOne({
             facultyid
+=======
+        var result = {};
+        let data = req.body.data.data;
+        if (data.email === 'admin' && data.password === 'admin') {
+            console.log("data >>", data);
+            result = {
+                token : "",
+                user_name: "testuser",
+                user_type: "string",
+                login_id: 1
+            }
+        } else {
+            throw new Error('Auth Error1');
+        }
+        res.status(200).json({
+            status: true,
+            message: 'login success!',
+            data: result
+>>>>>>> Stashed changes
         });
         if (!user)
             return res.status(400).json({
